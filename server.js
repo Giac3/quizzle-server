6b39c8ae7c93409bd108d6d1d8ca50d0115170ed
Giacomo Pessotto <giacomopesso@gmail.com>
2023-02-28 19:11:07 +0000

    added ignore

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3333
 app.use(cors())
 
 let time = 0;
