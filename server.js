f2299bc16b200f0962f8262ac41f4b5feabe8555
Giacomo Pessotto <giacomopesso@gmail.com>
2023-02-28 17:22:25 +0000

    node server stuff for quizzle

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+const PORT = process.env.PORT || 3000
+app.use(cors())
+
+let time = 0;
+
+let question = {};
+
+const getQuestion = () => {
+    setTimeout(() => {
+        fetch('https://the-trivia-api.com/api/questions?limit=1').then((res) => {
+            res.json().then((data) => {
+                
+                question = data
+                console.log(question)
+                getQuestion()
+            })
+            
+        })
+        
+    }, 30000);
+    
+}
+getQuestion()
+
+
+const timer = () => {
+    if (time === 30) {
+        time = 0
+    }
+    setTimeout(() => {
+        time+=1
+        timer()
+    }, 1000);
+}
+
+timer()
+
+app.get("/api", (req,res) => {
+    res.status(200).json({
+        time: time,
+        question: question
+    })
+})
+
+app.listen(PORT, () => console.log(`running on ${PORT}`))
