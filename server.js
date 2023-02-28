71726fc5ef0249f1efbd2a315414be940ce59f0e
Giacomo Pessotto <giacomopesso@gmail.com>
2023-02-28 19:33:26 +0000

    fixed node fetch

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const fetch = require('node-fetch')
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const PORT = process.env.PORT || 3333
 app.use(cors())
